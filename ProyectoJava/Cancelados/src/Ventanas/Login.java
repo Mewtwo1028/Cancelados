c87317c1d46f3136bf6286e446cf615b75ecbf16
Cesar Inzunsa <cesarinzunsa@hotmail.com>
2023-04-06 22:24:54 -0700

    #24

--- ProyectoJava/Cancelados/src/Ventanas/Login.java
+++ ProyectoJava/Cancelados/src/Ventanas/Login.java
@@ -1,19 +1,48 @@
 package Ventanas;
 
 import ArchivosBD.Conexion;
-import javax.swing.JOptionPane;
+import Código.FuncionesUtiles;
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
 
 /**
  *
  * @author osmar
  */
 public class Login extends javax.swing.JFrame {
-
-    Conexion con = new Conexion();
-
+    
     public Login() {
         initComponents();
+        inicializar();
+    }
+    
+    private void inicializar() {
+        //Configuracion ventana
         this.setLocationRelativeTo(null);
+        this.getContentPane().setBackground(Color.WHITE);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setTitle("Iniciar sesion");
+
+        //Imagenes
+        FuncionesUtiles funcionesUtiles = new FuncionesUtiles();
+        funcionesUtiles.colocarImagen("/Imagenes/LogoLetrasDegradado.png", jLabelLogoDegradado);
+        funcionesUtiles.colocarImagen("/Imagenes/bolitas.png", jLabelBolitas);
+
+        //Boton olvidar contraseña
+        btnForgottenPassword.setOpaque(false);
+        btnForgottenPassword.setContentAreaFilled(false);
+        btnForgottenPassword.setBorderPainted(false);
+
+        //Configurar jPanelCredenciales
+        jPanelCredenciales.setBackground(Color.WHITE);
+
+        //Boton ingresar
+        btnIngresar.setBackground(new Color(94, 29, 27));
+        btnIngresar.setForeground(Color.WHITE);
+
+        //Bienvenida
+        jLabelBienvenida.setForeground(new Color(94, 29, 27));
     }
 
     /**
@@ -25,56 +54,31 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jLabel3 = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
+        jPanelCredenciales = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        txtPassword = new javax.swing.JTextPane();
         jLabel2 = new javax.swing.JLabel();
         txtUsername = new javax.swing.JTextField();
         btnIngresar = new javax.swing.JButton();
+        txtPassword = new javax.swing.JPasswordField();
+        btnForgottenPassword = new javax.swing.JButton();
+        jLabelLogoDegradado = new javax.swing.JLabel();
+        jLabelBienvenida = new javax.swing.JLabel();
+        jLabelBolitas = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(1280, 720));
+        setPreferredSize(new java.awt.Dimension(1280, 720));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel3.setText("BIENVENIDO A CANCELADOS");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(235, 235, 235)
-                .addComponent(jLabel3)
-                .addContainerGap(288, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(43, Short.MAX_VALUE)
-                .addComponent(jLabel3)
-                .addGap(41, 41, 41))
-        );
-
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 680, -1));
-
-        jPanel2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jPanel2.setForeground(new java.awt.Color(255, 0, 51));
-        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jPanelCredenciales.setForeground(new java.awt.Color(255, 0, 51));
+        jPanelCredenciales.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setText("Username");
-        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 40, -1, -1));
-
-        jScrollPane1.setViewportView(txtPassword);
-
-        jPanel2.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 180, 210, 40));
+        jPanelCredenciales.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, -1, -1));
 
         jLabel2.setText("Contraseña");
-        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 150, -1, -1));
-        jPanel2.add(txtUsername, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, 210, 40));
-
-        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 150, 420, 320));
+        jPanelCredenciales.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 100, -1, -1));
+        jPanelCredenciales.add(txtUsername, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, 270, 40));
 
         btnIngresar.setText("Ingresar");
         btnIngresar.addActionListener(new java.awt.event.ActionListener() {
@@ -82,18 +86,49 @@ public class Login extends javax.swing.JFrame {
                 btnIngresarActionPerformed(evt);
             }
         });
-        getContentPane().add(btnIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 420, -1, -1));
+        jPanelCredenciales.add(btnIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 180, 270, 40));
+
+        txtPassword.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                txtPasswordKeyPressed(evt);
+            }
+        });
+        jPanelCredenciales.add(txtPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 120, 270, 40));
+
+        btnForgottenPassword.setText("Olvide mi contraseña");
+        jPanelCredenciales.add(btnForgottenPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 230, 270, -1));
+
+        getContentPane().add(jPanelCredenciales, new org.netbeans.lib.awtextra.AbsoluteConstraints(950, 200, 320, 290));
+        getContentPane().add(jLabelLogoDegradado, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 1, 730, 720));
+
+        jLabelBienvenida.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        jLabelBienvenida.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelBienvenida.setText("Bienvenido");
+        getContentPane().add(jLabelBienvenida, new org.netbeans.lib.awtextra.AbsoluteConstraints(1020, 140, -1, -1));
+        getContentPane().add(jLabelBolitas, new org.netbeans.lib.awtextra.AbsoluteConstraints(1120, 490, 248, 231));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void txtPasswordKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPasswordKeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+            iniciarSesion();
+        }
+    }//GEN-LAST:event_txtPasswordKeyPressed
 
     private void btnIngresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIngresarActionPerformed
+        iniciarSesion();
+    }//GEN-LAST:event_btnIngresarActionPerformed
+    
+    private void iniciarSesion() {
+        Conexion con = new Conexion();
         String username = txtUsername.getText();
         String pass = txtPassword.getText();
-
-        if (con.verificarLogin(username, pass).equals("")) {
-            JOptionPane.showMessageDialog(this, "ERROR! Usuario o contraseña no validos");
+        
+        if ((username.isBlank() | pass.isBlank()) | con.verificarLogin(username, pass).equals("")) {
+            DialogoEmergente dEmergente = new DialogoEmergente(this, true);
+            dEmergente.setTexto("ERROR! Usuario o\ncontraseña no validos");
+            dEmergente.setVisible(true);
         } else {
             if (con.verificarLogin(username, pass).equals("1")) {
                 PanelAdministrador a = new PanelAdministrador();
@@ -105,12 +140,8 @@ public class Login extends javax.swing.JFrame {
                 b.setVisible(true);
             }
         }
-
-    }//GEN-LAST:event_btnIngresarActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
+    }
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -144,14 +175,15 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnForgottenPassword;
     private javax.swing.JButton btnIngresar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextPane txtPassword;
+    private javax.swing.JLabel jLabelBienvenida;
+    private javax.swing.JLabel jLabelBolitas;
+    private javax.swing.JLabel jLabelLogoDegradado;
+    private javax.swing.JPanel jPanelCredenciales;
+    private javax.swing.JPasswordField txtPassword;
     private javax.swing.JTextField txtUsername;
     // End of variables declaration//GEN-END:variables
 }
