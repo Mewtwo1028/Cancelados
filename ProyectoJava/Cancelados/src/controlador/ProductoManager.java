f1e1430a623a7632f21fb1172c5be2350be17873
Cesar Inzunsa <cesarinzunsa@hotmail.com>
2023-04-15 14:02:29 -0700

    #60
    Temas pendientes a discutir como la optimización y validación de funciones; pendiente a discutir volviendo a clases.

---
+++ ProyectoJava/Cancelados/src/controlador/ProductoManager.java
@@ -0,0 +1,254 @@
+package controlador;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import modelo.Producto;
+
+public class ProductoManager {
+
+    private Conexion conexion;
+
+    public ProductoManager() {
+        conexion = new Conexion();
+    }
+
+    /**
+     * Consulta todos los productos almacenados en la base de datos y devuelve
+     * los resultados en un ArrayList de String[].
+     *
+     * @return ArrayList de String[] con los datos de todos los productos.
+     *
+     * @throws RuntimeException si se produce un error al consultar los
+     * productos
+     */
+    public ArrayList<Producto> consultarProductos() {
+        String consulta = "SELECT idProducto, nombre, descripcion, precioUnitario, stock, autor  FROM producto;";
+        ArrayList<Producto> resultado = new ArrayList<>();
+
+        try (ResultSet cursor = conexion.getConexion().prepareStatement(consulta).executeQuery()) {
+            Producto producto;
+            while (cursor.next()) {
+                producto = new Producto();
+
+                producto.setIdProducto(cursor.getInt(1));
+                producto.setNombre(cursor.getString(2));
+                producto.setDescripcion(cursor.getString(3));
+                producto.setPrecioUnitario(cursor.getFloat(4));
+                producto.setStock(cursor.getInt(5));
+                producto.setAutor(cursor.getString(6));
+
+                resultado.add(producto);
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error al consultar los productos", ex);
+        }
+        return resultado;
+    }
+
+    /**
+     * Actualiza los datos de un producto en la base de datos incluyendo la
+     * imagen.
+     *
+     * @param producto El objeto producto con los nuevos datos.
+     * @return true si la actualización se realizó correctamente, false en caso
+     * contrario.
+     * @throws RuntimeException si ocurre un error al modificar el producto.
+     */
+    public boolean modificarProductoConImagen(Producto producto) {
+        String consulta = "UPDATE producto SET nombre=?, descripcion=?, precioUnitario=?,imagen=?,stock=?, autor=? WHERE idProducto=?;";
+
+        try (PreparedStatement ps = conexion.getConexion().prepareStatement(consulta)) {
+
+            ps.setString(1, producto.getNombre());
+            ps.setString(2, producto.getDescripcion());
+            ps.setFloat(3, producto.getPrecioUnitario());
+            ps.setBlob(4, new FileInputStream(new File(producto.getImagen())));
+            ps.setInt(5, producto.getStock());
+            ps.setString(6, producto.getAutor());
+            ps.setInt(7, producto.getIdProducto());
+
+            return ps.executeUpdate() == 1;
+
+        } catch (SQLException | FileNotFoundException ex) {
+            throw new RuntimeException("Error al modificar el producto", ex);
+        }
+    }
+
+    /**
+     * Actualiza los datos de un producto en la base de datos sin incluir la
+     * imagen.
+     *
+     * @param producto El objeto producto con los nuevos datos.
+     * @return true si la actualización se realizó correctamente, false en caso
+     * contrario.
+     * @throws RuntimeException si ocurre un error al modificar el producto.
+     */
+    public boolean modificarProductoSinImagen(Producto producto) {
+        String consulta = "UPDATE producto SET nombre=?, descripcion=?, precioUnitario=?,stock=?, autor=? WHERE idProducto=?;";
+
+        try (PreparedStatement ps = conexion.getConexion().prepareStatement(consulta)) {
+
+            ps.setString(1, producto.getNombre());
+            ps.setString(2, producto.getDescripcion());
+            ps.setFloat(3, producto.getPrecioUnitario());
+            ps.setInt(4, producto.getStock());
+            ps.setString(5, producto.getAutor());
+            ps.setInt(6, producto.getIdProducto());
+
+            return ps.executeUpdate() == 1;
+
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error al modificar el producto", ex);
+        }
+    }
+
+    /**
+     * Consulta los nombres, precios unitarios y stock de todos los productos en
+     * la base de datos.
+     *
+     * @return ArrayList de String[] con los datos de todos los productos.
+     *
+     * @throwsRuntimeException si ocurre un error al consultar los productos en
+     * la base de datos.
+     */
+    public ArrayList<Producto> consultarNombres() {
+        String consulta = "SELECT idProducto, nombre, precioUnitario, stock FROM producto;";
+        ArrayList<Producto> resultado = new ArrayList<>();
+        Producto producto;
+
+        try (ResultSet cursor = conexion.getConexion().prepareStatement(consulta).executeQuery()) {
+
+            while (cursor.next()) {
+                producto = new Producto();
+                producto.setIdProducto(cursor.getInt(1));
+                producto.setNombre(cursor.getString(2));
+                producto.setPrecioUnitario(cursor.getFloat(3));
+                producto.setStock(cursor.getInt(4));
+                resultado.add(producto);
+            }
+
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error al consultar el producto", ex);
+        }
+        return resultado;
+    }
+
+    /*
+    public String consultarPrecio(Producto producto) {
+        String consulta = "SELECT precioUnitario FROM producto WHERE nombre=?;";
+
+        try {
+            Conexion con = new Conexion();
+            PreparedStatement ps = con.getConexion().prepareStatement(consulta);
+            ps.setString(1, String.valueOf(producto.getNombre()));
+            ResultSet cursor = ps.executeQuery();
+            if (cursor.next()) {
+                do {
+                    return cursor.getString(1);
+                } while (cursor.next());
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
+    }
+     */
+    /**
+     * Obtiene la imagen del producto desde la base de datos y la muestra en un
+     * objeto JLabel.
+     *
+     * @param producto el producto del cual se desea obtener la imagen.
+     * @param jlabel el objeto JLabel en el cual se mostrará la imagen.
+     *
+     * @return true si se pudo obtener y mostrar la imagen correctamente, false
+     * en caso contrario.
+     *
+     * @throws RuntimeException si ocurre algún error al obtener la imagen desde
+     * la base de datos.
+     */
+    public boolean obtenerImagen(Producto producto, JLabel jlabel) {
+        String consulta = "SELECT Imagen FROM producto WHERE idProducto=?;";
+
+        try (PreparedStatement cs = conexion.getConexion().prepareStatement(consulta)) {
+            cs.setInt(1, producto.getIdProducto());
+            ResultSet cursor = cs.executeQuery();
+
+            if (cursor.next()) {
+                byte[] imageData = cursor.getBytes("Imagen");
+                ImageIcon format = new ImageIcon(imageData);
+                Image img = format.getImage();
+                Image img2 = img.getScaledInstance(jlabel.getWidth(), jlabel.getHeight(), Image.SCALE_SMOOTH);
+                ImageIcon img3 = new ImageIcon(img2);
+                jlabel.setIcon(img3);
+                return true;
+            }
+
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error al obtener la imagen del producto", ex);
+        }
+        return false;
+    }
+
+    /**
+     * Inserta un nuevo producto en la base de datos.
+     *
+     * @param producto el producto que se desea insertar.
+     *
+     * @return true si se pudo insertar el producto correctamente, false en caso
+     * contrario.
+     *
+     * @throws RuntimeException si ocurre algún error al insertar el producto en
+     * la base de datos.
+     */
+    public boolean insertarProducto(Producto producto) {
+        String consulta = "INSERT INTO producto (nombre,descripcion,PrecioUnitario,Imagen,stock,Autor) VALUES (?,?,?,?,?,?);";
+
+        try (PreparedStatement ps = conexion.getConexion().prepareStatement(consulta);) {
+            ps.setString(1, producto.getNombre());
+            ps.setString(2, producto.getDescripcion());
+            ps.setString(3, String.valueOf(producto.getPrecioUnitario()));
+            ps.setBlob(4, new FileInputStream(new File(producto.getImagen())));
+            ps.setString(5, String.valueOf(producto.getStock()));
+            ps.setString(6, producto.getAutor());
+
+            return ps.executeUpdate() == 1;
+
+        } catch (SQLException | FileNotFoundException ex) {
+            throw new RuntimeException("Error al insertar el producto", ex);
+        }
+    }
+
+    /**
+     * Elimina un producto existente de la base de datos.
+     *
+     * @param producto el producto que se desea eliminar.
+     *
+     * @return true si se pudo eliminar el producto correctamente, false en caso
+     * contrario.
+     *
+     * @throws RuntimeException si ocurre algún error al eliminar el producto de
+     * la base de datos.
+     */
+    public boolean eliminarProducto(Producto producto) {
+        String operacion = "DELETE FROM producto WHERE idProducto=?;";
+
+        try (PreparedStatement ps = conexion.getConexion().prepareCall(operacion)) {
+
+            ps.setInt(1, producto.getIdProducto());
+
+            return ps.executeUpdate() == 1;
+
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error al eliminar el producto", ex);
+        }
+    }
+
+}
--- ProyectoJava/Cancelados/src/modelo/Producto.java
+++ ProyectoJava/Cancelados/src/modelo/Producto.java
@@ -1,21 +1,5 @@
 package modelo;
 
-import controlador.Conexion;
-import java.awt.Image;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.sql.CallableStatement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-
 public class Producto {
 
     private int idProducto;
@@ -35,6 +19,15 @@ public class Producto {
         this.idProducto = idProducto;
     }
 
+    public Producto(int idProducto, String nombre, String descripcion, float precioUnitario, int stock, String autor) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precioUnitario = precioUnitario;
+        this.stock = stock;
+        this.autor = autor;
+    }
+
     public Producto(String nombre, String descripcion, float precioUnitario, String imagen, int stock, String autor) {
         this.nombre = nombre;
         this.descripcion = descripcion;
@@ -42,187 +35,6 @@ public class Producto {
         this.imagen = imagen;
         this.stock = stock;
         this.autor = autor;
-    }
-
-    public Producto(int idProducto, String nombre, String descripcion, float precioUnitario, String imagen, int stock, String autor) {
-        this.idProducto = idProducto;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precioUnitario = precioUnitario;
-        this.imagen = imagen;
-        this.stock = stock;
-        this.autor = autor;
-    }
-
-    public ArrayList<Producto> consultarNombres() {
-        ArrayList<Producto> resultado = new ArrayList<>();
-        Producto producto;
-
-        ResultSet cursor;
-        String consulta = "SELECT idProducto, nombre, precioUnitario, stock FROM producto;";
-
-        try {
-            Conexion con = new Conexion();
-            cursor = con.getTransaccion().executeQuery(consulta);
-            if (cursor.next()) {
-                do {
-                    producto = new Producto();
-                    producto.setIdProducto(cursor.getInt(1));
-                    producto.setNombre(cursor.getString(2));
-                    producto.setPrecioUnitario(cursor.getFloat(3));
-                    producto.setStock(cursor.getInt(4));
-                    resultado.add(producto);
-                } while (cursor.next());
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return resultado;
-    }
-
-    public String consultarPrecio(Producto producto) {
-        String consulta = "SELECT precioUnitario FROM producto WHERE nombre=?;";
-
-        try {
-            Conexion con = new Conexion();
-            PreparedStatement cs = con.getConexion().prepareStatement(consulta);
-            cs.setString(1, String.valueOf(producto.getNombre()));
-            ResultSet cursor = cs.executeQuery();
-            if (cursor.next()) {
-                do {
-                    return cursor.getString(1);
-                } while (cursor.next());
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return "";
-    }
-
-    public ArrayList<String[]> consultarTodos() {
-        ArrayList<String[]> resultado = new ArrayList<>();
-
-        ResultSet cursor;
-        String consulta = "SELECT * FROM producto;";
-
-        try {
-            Conexion con = new Conexion();
-            cursor = con.getTransaccion().executeQuery(consulta);
-            if (cursor.next()) {
-                do {
-                    String[] renglon = {cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(6), cursor.getString(7)};
-                    resultado.add(renglon);
-                } while (cursor.next());
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return resultado;
-    }
-
-    public boolean obtenerImagen(Producto producto, JLabel jlabel) {
-        Conexion con = new Conexion();
-
-        String consulta = "SELECT Imagen FROM producto WHERE idProducto=?;";
-
-        try {
-            PreparedStatement cs = con.getConexion().prepareStatement(consulta);
-            cs.setString(1, String.valueOf(producto.getIdProducto()));
-
-            ResultSet cursor = cs.executeQuery();
-
-            if (cursor.next()) {
-
-                byte[] imageData = cursor.getBytes("Imagen");
-                ImageIcon format = new ImageIcon(imageData);
-                Image img = format.getImage();
-                Image img2 = img.getScaledInstance(jlabel.getWidth(), jlabel.getHeight(), Image.SCALE_SMOOTH);
-                ImageIcon img3 = new ImageIcon(img2);
-                jlabel.setIcon(img3);
-                return true;
-            }
-
-        } catch (SQLException ex) {
-            Logger.getLogger(Producto.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return false;
-    }
-
-    public boolean insertarProducto(Producto producto) {
-
-        File f = new File(producto.getImagen());
-
-        String consulta = "INSERT INTO producto (nombre,descripcion,PrecioUnitario,Imagen,stock,Autor) VALUES (?,?,?,?,?,?);";
-
-        try {
-            InputStream is = new FileInputStream(f);
-
-            Conexion con = new Conexion();
-
-            PreparedStatement cs = con.getConexion().prepareStatement(consulta);
-
-            cs.setString(1, producto.getNombre());
-            cs.setString(2, producto.getDescripcion());
-            cs.setString(3, String.valueOf(producto.getPrecioUnitario()));
-
-            cs.setBlob(4, is);
-
-            cs.setString(5, String.valueOf(producto.getStock()));
-            cs.setString(6, producto.getAutor());
-
-            cs.execute();
-
-            return true;
-
-        } catch (SQLException | FileNotFoundException e) {
-            Logger.getLogger(Producto.class.getName()).log(Level.SEVERE, null, e);
-        }
-        return false;
-    }
-
-    public boolean modificarProducto(Producto producto) {
-        File f = new File(producto.getImagen());
-        Conexion con = new Conexion();
-        String consulta = "UPDATE producto SET nombre=?, descripcion=?, precioUnitario=?,imagen=?,stock=?, autor=? WHERE idProducto=?;";
-
-        try {
-            InputStream is = new FileInputStream(f);
-            CallableStatement cs = con.getConexion().prepareCall(consulta);
-
-            cs.setString(1, producto.getNombre());
-            cs.setString(2, producto.getDescripcion());
-            cs.setString(3, String.valueOf(producto.getPrecioUnitario()));
-            //cs.setString(4, String.valueOf(producto.getImagen()));
-
-            cs.setBlob(4, is);
-
-            cs.setString(5, String.valueOf(producto.getStock()));
-            cs.setString(6, producto.getAutor());
-            cs.setString(7, String.valueOf(producto.getIdProducto()));
-
-            cs.execute();
-
-            return true;
-        } catch (NumberFormatException | SQLException | FileNotFoundException e) {
-            return false;
-        }
-    }
-
-    public boolean eliminarProducto(Producto producto) {
-        Conexion conexion = new Conexion();
-        String operacion = "DELETE FROM producto WHERE idProducto=?;";
-
-        try {
-            CallableStatement cs = conexion.getConexion().prepareCall(operacion);
-
-            cs.setString(1, String.valueOf(producto.getIdProducto()));
-
-            cs.execute();
-
-            return true;
-        } catch (SQLException ex) {
-            return false;
-        }
     }
 
     public int getIdProducto() {
@@ -280,12 +92,12 @@ public class Producto {
     public void setAutor(String autor) {
         this.autor = autor;
     }
-    
-    public void setImporte(float importe){
+
+    public void setImporte(float importe) {
         this.importe = importe;
     }
-    
-    public float getImporte(){
+
+    public float getImporte() {
         return importe;
     }
 
